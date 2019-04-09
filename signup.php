2997cc333bbc6ab3bd17fc8fff5868e483f9f4ad
isalk <isalkorale@hotmail.com>
2019-04-09 14:44:52 +0800

    Sign Up Page Password match validation

--- signup.php
+++ signup.php
@@ -37,20 +37,20 @@ if(isset($_SESSION["email"])){
     </div>
     <div class="row content">
         <div class="col-xs-12">
-            <form method="POST" action="newregister.php">
+            <form method="POST" name=validateregister action="newregister.php">
                 <div id="login">
                     <p><label for="fullname">Full name :</label></p>
-                    <p><input type="text" name="full_name" id="fullname" required/></p>
+                    <p><input type="text" name="full_name" id="fullname" required /></p>
                     <p><label for="Email">Enter your Email :</label></p>
-                    <p><input type="email" name="Email" id="Email" required/></p>
+                    <p><input type="email" name="Email" id="Email" required /></p>
                     <p><label for="ReEmail">Re-enter your Email :</label></p>
-                    <p><input type="email" name="ReEmail" id="ReEmail" required/></p>
+                    <p><input type="email" name="ReEmail" id="ReEmail" required /></p>
                     <p><label for="Phone">Phone Number :</label></p>
-                    <p><input type="text" name="phone" id="phone"required /></p>
+                    <p><input type="text" name="phone" id="phone" required /></p>
                     <p><label for="Password">Enter your Password :</label></p>
-                    <p><input type="password" name="Password" id="Password" required/></p>
+                    <p><input type="password" name="Password" id="Password" required /></p>
                     <p><label for="RePassword">Re-enter your Password :</label></p>
-                    <p><input type="password" name="RePassword" id="RePassword" required/></p>
+                    <p><input type="password" name="RePassword" id="RePassword" required /></p>
                 </div>
                 <p><input type="submit" class="btn btn-success" value="Register" /> <input type="reset" class="btn btn-success" value="Reset" /></p>
             </form>
@@ -67,8 +67,25 @@ if(isset($_SESSION["email"])){
 <script src="js/bootstrap.min.js"></script>
 <!-- Basic AngularJS -->
 <script src="js/angular.min.js"></script>
-    
-     <?php
+
+<script type="text/javascript">
+    window.onload = function() {
+        document.getElementById("Password").onchange = validatePassword;
+        document.getElementById("RePassword").onchange = validatePassword;
+    }
+
+    function validatePassword() {
+        var pass2 = document.getElementById("RePassword").value;
+        var pass1 = document.getElementById("Password").value;
+        if (pass1 != pass2)
+            document.getElementById("RePassword").setCustomValidity("Passwords Don't Match");
+        else
+            document.getElementById("RePassword").setCustomValidity('');
+    }
+
+</script>
+
+<?php
 		include ('footer.php');
 	?>
 
