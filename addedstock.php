585c765fa56b39d8bbfd7c24af2a0f03db5bb570
gohnigel <gohnigel.ng@gmail.com>
2019-04-21 12:57:18 +0800

    Added extra fields for inventory items

--- addedstock.php
+++ addedstock.php
@@ -8,20 +8,25 @@ $id = $_POST["prod_id"];
 $inventory = $_POST["prod_inventory"];
 $qty = $_POST["prod_qty"];
 $price = $_POST['prod_price'];
+$desc = $_POST["prod_desc"];
+$colour = $_POST["prod_colour"];
 
 echo $id;
 echo $inventory;
 echo $qty;
 echo $price;
+echo $desc;
+echo $colour;
 
 
-if($mysqli->query("INSERT INTO stock (`prod_id`, `prod_name`, `qty`, `price`) VALUES('$id', '$inventory', '$qty', '$price')")){
+if($mysqli->query("INSERT INTO stock (`prod_id`, `prod_name`, `qty`, `price`, `desc`, `colour`) VALUES('$id', '$inventory', '$qty', '$price', '$desc', '$colour')")){
     echo"Added Success";
     header("Location: allstock.php");
 }
 else
 {
-
+    echo"Cannot add";
+    header("Location: addstock.php");
 
 }
 
