d51ed12d399cc31a2f35a671b534e068a7c9f3b3
ignatius5745 <100065630@students.swinburne.edu.my>
2019-04-23 17:37:52 +0800

    added more content to shop page

--- shop.php
+++ shop.php
@@ -33,7 +33,7 @@ if($_SESSION["role"]!="client") {
 <body>
     <div class="row content">
         <div class="col-xs-12">
-    <h1>Shop</h1>
+            <h1>Shop</h1>
             <?php
           $i=0;
           $product_id = array();
@@ -48,15 +48,18 @@ if($_SESSION["role"]!="client") {
 
             while($obj = $result->fetch_object()) {
 
-              echo '<div class="large-4 columns">';
-              echo '<p><h3 style="margin-top:80px">'.$obj->product_name.'</h3></p>';
-              echo '<p><strong>Product Code</strong>: '.$obj->product_code.'</p>';
+              echo '<div class="col-xs-3">';
+              echo '<p><h3 style="margin-top:80px">'.$obj->prod_name.'</h3></p>';
+              echo '<p><strong>Product Code</strong>: '.$obj->prod_id.'</p>';
+                echo '<p><strong>Brand</strong>: '.$obj->brand.'</p>';
               echo '<p><strong>Qty Available</strong>: '.$obj->qty.'</p>';
               echo '<p><strong>Price</strong>: '.$obj->price.'</p>';
+                echo '<input type="text" placeholder="enter your quantity">';
+                
 
 
               if($obj->qty > 0){
-                echo '<p><a href="update-cart.php?action=add&id='.$obj->product_code.'"><input type="submit" value="Reserve" style="clear:both; background: #0078A0; border: none; color: #fff; font-size: 1em; padding: 10px;" /></a></p>';
+                echo '<p><a href="update-cart.php?action=add&id='.$obj->prod_id.'"><input type="submit" value="Reserve" style="clear:both; background: #0078A0; border: none; color: #fff; font-size: 1em; margin: 10px;padding: 10px;" /></a></p>';
               }
               else {
                 echo 'Out Of Stock!';
@@ -76,9 +79,9 @@ if($_SESSION["role"]!="client") {
           ?>
         </div>
     </div>
-    
-    
-        <!-- jQuery – required for Bootstrap's JavaScript plugins) -->
+
+
+    <!-- jQuery – required for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery.min.js"></script>
     <!-- All Bootstrap plug-ins file -->
     <script src="js/bootstrap.min.js"></script>
