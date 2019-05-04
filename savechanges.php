f9f724336d71f2b984e485ae3ac7eda70fb82d7d
ignatius5745 <100065630@students.swinburne.edu.my>
2019-05-04 15:15:20 +0800

    Revert "Merge branch 'master' of https://github.com/gohnigel/DevelopmentProject2"
    This reverts commit 7ce461baee570e18ef895ea4b9f553234b19d9fd, reversing
    changes made to d334c339bc2db3253f7c89ff36c375a3134b9800.

--- savechanges.php
+++ savechanges.php
@@ -5,14 +5,15 @@ include 'config.php';
 $fullname = $_POST['full_name'];
 $email = $_POST['Email'];
 $phone = $_POST['phone'];
-$password = $_POST['password'];
+$image = $_FILES['image']['name'];
 
-$result = $mysqli->query("UPDATE users SET full_name='$fullname', Email='$email', phone='$phone', password='$password' WHERE Email='$email'");
+$result = $mysqli->query("UPDATE users SET full_name='$fullname', Email='$email', phone='$phone', image='$image' WHERE Email='$email'");
 
 
 if ($result){
 	echo 'Changes saved';
 	echo '<br/>';
+    move_uploaded_file($_FILES["image"]["tmp_name"], 'images/'.$image);
     header ("location:profile.php");
 }
 else
