def0cfbeecd2224315182563dbd7ff5faf6c0857
ignatius5745 <100065630@students.swinburne.edu.my>
2019-05-08 15:16:12 +0800

    Added notification low stock

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Salon</title>
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="css/gui.css" />
-        <link rel="stylesheet" type="text/css" href="css/foundation.css" />
+    <link rel="stylesheet" type="text/css" href="css/foundation.css" />
 
 </head>
 <header>
@@ -23,51 +23,92 @@
 
 <body>
 
-  <div class="container-fluid">
-    <div class="row content">
-      <div class="col-xs-12">
-        <h3>Welcome to Hair Salon Online Booking</h3>
-        <p>We have many features to work on for this website itself</p>
-      </div>
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-xs-12">
+                <?php
+          $user = $_SESSION["email"];
+          $result = $mysqli->query("SELECT * from products");
+          if($result) {
+            while($obj = $result->fetch_object()) {
+                if($obj->qty < 5)
+                {
+               echo '<div class="alert warning" style="padding: 20px;
+                        background-color: orange;
+                        color: white;
+                        margin-bottom: 15px;">';
+                echo '<span class="closebtn" style="margin-left: 15px;
+                        color: white;
+                        font-weight: bold;
+                        float: right;
+                        font-size: 22px;
+                        line-height: 20px;
+                        cursor: pointer;
+                        transition: 0.3s;">&times;</span>';
+                echo'<strong>Warning! </strong>'.$obj->product_name.' is in Low stock';
+                echo '</div>';
+                }
+          }
+            }
+        ?>
+                <h3>Welcome to Hair Salon Online Booking</h3>
+                <p>We have many features to work on for this website itself</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="slideshow-container">
+
+                    <div class="mySlides fade">
+                        <div class="numbertext">1 / 3</div>
+                        <img src="images/slide1.jpg" style="width:100%">
+                        <div class="text">Caption Text</div>
+                    </div>
+
+                    <div class="mySlides fade">
+                        <div class="numbertext">2 / 3</div>
+                        <img src="images/slide12.jpg" style="width:100%">
+                        <div class="text">Caption Two</div>
+                    </div>
+
+                    <div class="mySlides fade">
+                        <div class="numbertext">3 / 3</div>
+                        <img src="images/slide13.jpg" style="width:100%">
+                        <div class="text">Caption Three</div>
+                    </div>
+
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+                    <div class="text-center">
+                        <span class="dot" onclick="currentSlide(1)"></span>
+                        <span class="dot" onclick="currentSlide(2)"></span>
+                        <span class="dot" onclick="currentSlide(3)"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row content">
+
+        </div>
     </div>
-    <div class="row">
-     <div class="col-xs-12">
-      <div class="slideshow-container">
 
-        <div class="mySlides fade">
-          <div class="numbertext">1 / 3</div>
-          <img src="images/slide1.jpg" style="width:100%">
-          <div class="text">Caption Text</div>
-        </div>
+    <script>
+        var close = document.getElementsByClassName("closebtn");
+        var i;
 
-        <div class="mySlides fade">
-          <div class="numbertext">2 / 3</div>
-          <img src="images/slide12.jpg" style="width:100%">
-          <div class="text">Caption Two</div>
-        </div>
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function() {
+                var div = this.parentElement;
+                div.style.opacity = "0";
+                setTimeout(function() {
+                    div.style.display = "none";
+                }, 600);
+            }
+        }
 
-        <div class="mySlides fade">
-          <div class="numbertext">3 / 3</div>
-          <img src="images/slide13.jpg" style="width:100%">
-          <div class="text">Caption Three</div>
-        </div>
-
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-        <div class="text-center">
-          <span class="dot" onclick="currentSlide(1)"></span> 
-          <span class="dot" onclick="currentSlide(2)"></span> 
-          <span class="dot" onclick="currentSlide(3)"></span> 
-        </div>
-      </div>
-    </div>
-  </div>
-
-    <div class="row content">
- 
-    </div> 
-  </div>
+    </script>
 
 
 
@@ -82,7 +123,8 @@
     <script type="text/javascript" src="js/slideshow.js"></script>
 
 </body>
-    <?php
+<?php
 		include ('footer.php');
 	?>
+
 </html>
