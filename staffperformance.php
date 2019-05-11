82dfc4ddac778fe5bc2f0d43b625a9aeef9b1f10
ignatius5745 <100065630@students.swinburne.edu.my>
2019-05-11 17:15:02 +0800

    Added dummy data and graph to staff performance

--- staffperformance.php
+++ staffperformance.php
@@ -10,6 +10,10 @@ if(!isset($_SESSION["email"])) {
 if($_SESSION["role"]!="admin") {
   header("location:index.php");
 }
+$result = $mysqli->query("SELECT full_name, COUNT(full_name) as StaffCount
+FROM booking
+GROUP BY full_name");
+
 ?>
 <!DOCTYPE html>
 
@@ -19,7 +23,8 @@ if($_SESSION["role"]!="admin") {
     <title>Salon</title>
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="css/gui.css" />
-        <link rel="stylesheet" type="text/css" href="css/foundation.css" />
+    <link rel="stylesheet" type="text/css" href="css/foundation.css" />
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
 
 </head>
@@ -42,6 +47,7 @@ if($_SESSION["role"]!="admin") {
             <?php echo '<h3>Hi ' .$_SESSION['full_name'] .'</h3>'; ?>
             <h5> Staff Mode</h5>
             <p><b>Your Currently viewing all Staff Performance</b></p>
+            <div id="piechart" style="width: 900px; height: 500px;"></div>
         </div>
     </div>
     <div class="row">
@@ -54,12 +60,10 @@ if($_SESSION["role"]!="admin") {
                 </tr>
                 <?php
           $user = $_SESSION["email"];
-          $result = $mysqli->query("SELECT users.full_name, (SELECT COUNT(full_name) FROM booking) as StaffCount
-FROM booking
-INNER JOIN users ON booking.full_name = users.full_name
-GROUP BY 1");
-          if($result) {
-            while($obj = $result->fetch_object()) {
+        $result1 = $mysqli->query("SELECT full_name, COUNT(full_name) as StaffCount FROM booking GROUP BY full_name");
+                
+          if($result1) {
+            while($obj = $result1->fetch_object()) {
 			  echo '<tr>';
               echo '<td>'.$obj->full_name.'</td>';
               echo '<td>'.$obj->StaffCount.'</td>';
@@ -72,6 +76,33 @@ GROUP BY 1");
         </div>
     </div>
   </div>
+    
+    <script type="text/javascript">
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable([
+                ['Gender', 'Number'],
+                <?php  
+                          while($row = mysqli_fetch_array($result))  
+                          {  
+                               echo "['".$row["full_name"]."', ".$row["StaffCount"]."],";  
+                          }  
+                          ?>
+            ]);
+            var options = {
+                title: 'Percentage of Staff Chosen',
+                is3D: true,
+                pieHole: 0.4
+            };
+            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+            chart.draw(data, options);
+        }
+
+    </script>
     <!-- jQuery – required for Bootstrap's JavaScript plugins) -->
     <script src="js/jquery.min.js"></script>
     <!-- All Bootstrap plug-ins file -->
@@ -82,6 +113,7 @@ GROUP BY 1");
     <script src="js/jquery-2.1.3.js"></script>
     <script src="js/pdfFromHTML.js"></script>
     <script src="js/jspdf.js"></script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 
 </body>
 <?php
